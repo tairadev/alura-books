102c3c562a6652dc65228d5626183d8ebebdc249
Guilherme Henrique Taira <53182990+tairadev@users.noreply.github.com>
2025-01-25 10:34:42 -0300

    Rota de livro por id

--- api/controllers/book.js
+++ api/controllers/book.js
@@ -1,4 +1,4 @@
-const { getAllBooks } = require('../services/book');
+const { getAllBooks, getBookById } = require('../services/book');
 
 function getBooks(req, res) {
   try {
@@ -9,6 +9,19 @@ function getBooks(req, res) {
   }
 }
 
+function getSpecificBook(req, res) {
+  try {
+    const book = getBookById(req.params.id);
+    if (!book) {
+      res.status(404).json({ message: 'Book not found' });
+    }
+    res.json(book);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}
+
 module.exports = {
   getBooks,
+  getSpecificBook,
 };
--- api/services/book.js
+++ api/services/book.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
 
-function getAllBooks(req, res) {
+function getAllBooks() {
   const books = JSON.parse(fs.readFileSync('data/books.json', 'utf8'));
   return books;
 }
 
+function getBookById(id) {
+  const books = getAllBooks();
+  const book = books.find((book) => book.id === id);
+  return book;
+}
+
 module.exports = {
   getAllBooks,
+  getBookById,
 };
