18b678c54dba9ab71117a22bd196d0551993b28b
Guilherme Henrique Taira <53182990+tairadev@users.noreply.github.com>
2025-01-26 23:08:18 -0300

    Serviço de livros favoritos

---
+++ api/services/favorite.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const { getAllBooks } = require('./book');
+
+function getAllFavorites() {
+  const books = JSON.parse(fs.readFileSync('data/favorites.json', 'utf8'));
+  return books;
+}
+
+function deleteFavoriteById(id) {
+  const currentFavorites = getAllFavorites();
+
+  const filteredFavorites = currentFavorites.filter(
+    (favorite) => favorite.id !== id
+  );
+
+  fs.writeFileSync(
+    'data/favorites.json',
+    JSON.stringify(filteredFavorites, null, 2)
+  );
+}
+
+function insertFavorite(id) {
+  const books = getAllBooks();
+  const favorites = getAllFavorites();
+
+  const insertedBook = books.find((b) => b.id === id);
+  const newFavorites = [...favorites, insertedBook];
+
+  fs.writeFileSync(
+    'data/favorites.json',
+    JSON.stringify(newFavorites, null, 2)
+  );
+}
+
+module.exports = {
+  getAllFavorites,
+  insertFavorite,
+  deleteFavoriteById,
+};
